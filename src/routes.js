c08223bac3db2e83288738b881175b853e73c7f6
gilbertohnrq <gilbertohnrq@gmail.com>
2019-10-28 00:44:50 -0300

    Adding model loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello omni' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'User teste',
+    email: 'userteste@gmail.com',
+    password_hash: 'testeste123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
